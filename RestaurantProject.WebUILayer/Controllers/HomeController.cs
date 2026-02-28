c1efa8d6e9cea2f69d37fec889160891fd7c48a3
tunadeveloper <tunabusiness25@gmail.com>
2026-02-28 17:53:55 +0300

    Updated

--- RestaurantProject.WebUILayer/Controllers/HomeController.cs
+++ RestaurantProject.WebUILayer/Controllers/HomeController.cs
@@ -10,16 +10,5 @@ namespace RestaurantProject.WebUILayer.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
