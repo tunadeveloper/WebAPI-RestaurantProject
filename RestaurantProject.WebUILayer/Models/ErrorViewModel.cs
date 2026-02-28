be837a1dee31a2405c36b02e1ec59810c26407ab
tunadeveloper <tunabusiness25@gmail.com>
2026-02-28 17:50:54 +0300

    Updated

---
+++ RestaurantProject.WebUILayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantProject.WebUILayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
