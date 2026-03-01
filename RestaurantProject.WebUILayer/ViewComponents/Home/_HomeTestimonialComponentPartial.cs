b9b1e094cdd9e271da55058d195d41bc9fe15b22
tunadeveloper <tunabusiness25@gmail.com>
2026-03-01 16:28:35 +0300

    Updated

--- RestaurantProject.WebUILayer/ViewComponents/Home/_HomeTestimonialComponentPartial.cs
+++ RestaurantProject.WebUILayer/ViewComponents/Home/_HomeTestimonialComponentPartial.cs
@@ -1,9 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantProject.WebUILayer.DTOs.TestimonialDTOs;
+using System.Threading.Tasks;
 
 namespace RestaurantProject.WebUILayer.ViewComponents.Home
 {
     public class _HomeTestimonialComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
-    }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _HomeTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7052/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+}
 }
